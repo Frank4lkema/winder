25ea1932f74eef48b23d01a2e8ee50172cba3239
Frank Alkema <frank@aeroscan.nl>
2022-02-16 20:07:44 +0100

    Removed the clear data method

--- app/lib/services/collect_wind_data.rb
+++ app/lib/services/collect_wind_data.rb
@@ -9,9 +9,9 @@ module  Services
     end
 
     def call
-      # clear_data_base
-      windguru_data = HTTParty.get('https://www.windguru.net/int/iapi.php?q=forecast&id_model=52&rundef=2022021512x0x42x0x42&initstr=2022021512&id_spot=48299&WGCACHEABLE=21600&cachefix=52.451x4.564x0')
-      windguru_data = HTTParty.get('https://www.windguru.net/int/iapi.php?q=forecast&id_model=52&rundef=2022021600x0x42x0x42&initstr=2022021600&id_spot=48299&WGCACHEABLE=21600&cachefix=52.451x4.564x0')
+      # windguru_data2 = HTTParty.get('https://www.windguru.net/int/iapi.php?q=forecast&id_model=52&rundef=2022021506x0x36x0x36&initstr=2022021506&id_spot=48299&WGCACHEABLE=21600&cachefix=52.451x4.564x0')
+      # windguru_data =  HTTParty.get('https://www.windguru.net/int/iapi.php?q=forecast&id_model=52&rundef=2022021606x0x36x0x36&initstr=2022021606&id_spot=48299&WGCACHEABLE=21600&cachefix=52.451x4.564x0')
+      windguru_data =  HTTParty.get('https://www.windguru.net/int/iapi.php?q=forecast&id_model=52&rundef=2022021706x0x36x0x36&initstr=2022021706&id_spot=48299&WGCACHEABLE=21600&cachefix=52.451x4.564x0')
       forcast_data = windguru_data['fcst']
       wind_speed_objects = forcast_data['WINDSPD'].map.with_index do |wind_speed, i|
         wind_speed > 15 ? { wind_speed: wind_speed, hour: i } : nil
@@ -21,10 +21,6 @@ module  Services
     end
 
     private
-
-    def clear_data_base
-      WindForecast.destroy_all
-    end
 
     def create_wind_forecasts(objects,initial_hour)
       date = Date.today + initial_hour.hours
